bd014c4f125d2d1d3fd27bd413b73dbe18dd1c45
Kalu Wisdom <kaluwisdom66@gmail.com>
2024-11-04 03:00:20 -0800

    initial commit

---
+++ js.intro.js
@@ -0,0 +1,97 @@
+
+// let
+// var
+// const
+
+// Array
+let data = [ "two" , 3, "four"]
+alert({0})
+// ARRAY METHOD
+let newData = data. push
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS
+// ARROW FUNCTIONS
+// const minNumber =  (a, b)=>{
+    // body of the function
+
+// if (a<b) {
+    // alert(a)
+// } else {
+    // alert(b)
+}
+
+
+
+
+
+}
+
+// minNumber(10,11)
+
+
+
+
+
+// NAME FUNCTIONS
+// Definition of Function
+// function MaxNumber(a, b){
+    //function body
+    // if (a>b) {
+    //   alert(a)
+    // } else {
+    //   alert(b)
+    // }
+//   }
+  
+  
+  
+  
+  // Call Our Function
+//   MaxNumber(0, 1)
+  
+  
+//   const Surname = "samuel"; String
+
+
+ 
+// while (Emeka Good Morning) {}
+    
+// alert(Emeka + GoodMorning)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

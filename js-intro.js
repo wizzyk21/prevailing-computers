54b55f6fdb2f53103c49e4f3fc2a40adb77e1c22
Kalu Wisdom <kaluwisdom66@gmail.com>
2024-11-14 04:25:05 -0800

    second commit

---
+++ js-intro.js
@@ -0,0 +1,164 @@
+
+// let
+// var
+// // const
+
+// // Array
+// let data = [ "two" , 3, "four"]
+// // alert({0})
+// // ARRAY METHOD
+// let newData = data. push
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS
+// ARROW FUNCTIONS
+// const minNumber =  (a, b)=>{
+    // body of the function
+
+// if (a<b) {
+    // alert(a)
+// } else {
+    // alert(b)
+// }
+
+// const GoodMorning = (a, b)=>{
+     
+// prompt("What is your name"?  )
+//     alert("What is your name"? any)
+// }
+
+// }
+
+// minNumber(10,11)
+
+
+
+
+
+// NAME FUNCTIONS
+// Definition of Function
+// function MaxNumber(a, b){
+//     function body
+//     if (a>b) {
+//       alert(a)
+//     } else {
+//       alert(b)
+//     }
+//   }
+  
+  
+  
+  
+//   Call Our Function
+//   MaxNumber(0, 1)
+  
+  
+//   const Surname = "samuel"; String
+
+
+ 
+// while (Emeka Good Morning) {}
+    
+// alert(Emeka + GoodMorning)
+
+
+
+// OBJECTS
+// This is the physical rep of a thing.
+
+
+// CLASS
+// This is the blueprint for building an object
+
+
+// A house is the object
+// and the building plan is the class
+
+
+// KEY COMPONENTS OF OOP 
+// (Object Oriented Programming)
+// 1. class: The blueprint for building an object
+// 2. object: is an instance of a class
+// 3.properties: variables and values that a class or an object will have
+// 4. Methods: are function inside an object or a class 
+
+
+// class House {
+    
+//     // properties of the house class
+    
+//     furnitures;
+//     television;
+//     kitchen_util;
+//     room;
+//     refrigerator;
+
+//     // methods are functions inside a class
+    
+//     noisy(){
+//         alert("The house is noisy")
+//     };
+
+//     calm(){
+//         alert("The house is calm")
+//     }
+
+// }
+
+
+
+// const Duplex = new House();
+
+// Duplex.noisy()
+
+//  Duplex.furnitures = 5;
+
+ class Food {
+    //    properties of the food class
+
+    protein;
+    carbonhydrate;
+    vitamin;
+
+    //    methods inside the food class
+
+      Bean(){
+               alert("The food is Bean")
+       };
+       Rice(){
+                     alert("The food is Rice")
+       };
+      Orange(){
+               alert("The food is Orange")
+       };
+ }
+     const Garri = new Quality(); 
+
+     Garri.Rice()
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
